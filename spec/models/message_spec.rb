97672c1c56062821921f67d2aa002a2a588feef1
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-03 14:24:54 -0500

    Add and pass tests for message model.

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "message model" do
+  let(:user) { User.create!(username:"jay", email:"jay@gmail.com", password: "1234") }
+  let(:user2) { User.create!(username:"jess", email:"jess@gmail.com", password: "password") }
+  let(:convo) { Conversation.create(sender_id: user.id, recipient_id: user2.id) }
+  let(:msg1) { Message.create(conversation_id: convo.id, body: "Hey, wassup?", user_id: user.id, read: true)}
+  let(:msg2) { Message.create(conversation_id: convo.id, body: "Chilling. What are you up to?", user_id: user2.id)}
+
+  let(:saved_user) { User.find_by(username:"jay") }
+  let(:saved_user2) { User.find_by(username:"jess") }
+
+  it "has a conversation" do
+    expect(msg1.conversation_id).to eq convo.id
+  end
+
+  it "has a body" do
+    expect(msg1.body).to eq "Hey, wassup?"
+  end
+
+  it "has a sender" do
+    expect(msg1.user_id).to eq user.id
+  end
+
+  it "says whether or not it's been read" do
+    expect(msg1.read).to eq true 
+  end
+
+end
