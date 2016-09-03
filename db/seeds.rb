0f65dd0c7f7cd59e7e11a949910cfcd9b05b1245
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-03 13:33:53 -0500

    Add message and conversation seed data. Add to interest spec.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,6 @@ user3 = User.create(username: 'Ross', email: "ross123@hotmail.com", password_dig
 User.create(username: 'Charlie', email: "charlie@gmail.com", password_digest: "abc123")
 User.create(username: 'Lucy', email: "bluedresses4va@gmail.com", password_digest: "abc123")
 
-
-
 i1 = Interest.create(name: "Sports")
 i2 = Interest.create(name: "Music")
 i3 = Interest.create(name: "Food")
@@ -29,3 +27,11 @@ user2.interests.create(name: "Food")
 
 user3.interests.create(name: "Food")
 user3.interests.create(name: "Music")
+
+convo1 = Conversation.create(sender_id: user1.id, recipient_id: user2.id)
+message1 = Message.create(conversation_id: convo1.id, body: "Hey, wassup?", user_id: user1.id)
+message2 = Message.create(conversation_id: convo1.id, body: "Chilling. What are you up to?", user_id: user2.id)
+
+convo2 = Conversation.create(sender_id: user2.id, recipient_id: user3.id)
+message1 = Message.create(conversation_id: convo2.id, body: "Hey, wassup?", user_id: user2.id)
+message2 = Message.create(conversation_id: convo2.id, body: "Chilling. What are you up to?", user_id: user3.id)
