fcdbebae660b1ff49b7c27c252072152160ba2b2
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-03 16:29:04 -0500

    Add failing tests for Users controller.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  let(:user) { User.create!(username:"jay", email:"jay@gmail.com", password: "1234") }
+let(:user) { User.create!(username:"jay", email:"jay@gmail.com", password: "1234") }
 
-    context "when valid params are passed" do
-      let :params do
-          { :email => user.email, :password => user.password }
-        end
-
-      it "logs in the correct user and sets a session" do
-        post :create, params
-        expect(response).to have_http_status 302
-        expect(session[:user_id]).to eq(user.id)
-        expect(current_user.id).to eq(user.id)
-        expect(flash[:notice]).to eq("Logged in!")
-        expect(response).to redirect_to root_path
+  context "when valid params are passed" do
+    let :params do
+        { :email => user.email, :password => user.password }
       end
+
+    it "logs in the correct user and sets a session" do
+      post :create, params
+      expect(response).to have_http_status 302
+      expect(session[:user_id]).to eq(user.id)
+      expect(current_user.id).to eq(user.id)
+      expect(flash[:notice]).to eq("Logged in!")
+      expect(response).to redirect_to root_path
     end
+  end
 
-    context "when invalid params are passed" do
-      let :params do
-          { :email => "test@test.com", :password => "wrong" }
-        end
-      it "returns you to the login page" do
-        post :create, params
-        expect(flash[:alert]).to eq("Invalid email or password")
-        expect(response).to render_template("sessions/new", "layouts/application")
+  context "when invalid params are passed" do
+    let :params do
+        { :email => "test@test.com", :password => "wrong" }
       end
+    it "returns you to the login page" do
+      post :create, params
+      expect(flash[:alert]).to eq("Invalid email or password")
+      expect(response).to render_template("sessions/new", "layouts/application")
     end
+  end
 
-      it "logs you out and clears out the session" do
-        post :destroy
-        expect(session[:user_id]).to eq nil
-        expect(flash[:notice]).to eq("Logged out!")
-        expect(response).to redirect_to root_path
-      end
+    it "logs you out and clears out the session" do
+      post :destroy
+      expect(session[:user_id]).to eq nil
+      expect(flash[:notice]).to eq("Logged out!")
+      expect(response).to redirect_to root_path
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,119 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+let(:user) { User.create!(username:"jay", email:"jay@gmail.com", password: "1234") }
+let(:user2) { User.create!(username:"jess", email:"jess@gmail.com", password: "password") }
+
+  describe "GET #index" do
+    before(:each) do
+      session[:user_id] = user.id
+    end
+    it "responds with status code 200" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status 200
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #new" do
+    it "responds with status code 200" do
+      get :new
+      expect(response).to be_success
+      expect(assigns(:user)).to be_a_new User
+      expect(response).to have_http_status 200
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET #show" do
+    it "responds with status code 200" do
+      get :show, {id: user.id}
+      expect(response).to be_success
+      expect(assigns(:user)).to eq(user)
+      expect(response).to have_http_status 200
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "GET #edit" do
+    it "responds with status code 200" do
+      get :edit, {id: user.id}
+      expect(response).to be_success
+      expect(assigns(:user)).to eq(user)
+      expect(response).to have_http_status 200
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "POST #create" do
+    context "when valid params are passed" do
+      let :params do
+        {user: {username: "tom4", email: "t4tom@test.com", password: "tomdancer"}}
+      end
+
+      it "responds with status code 302" do
+        expect{post(:create, params)}.to change(User, :count).by(1)
+        expect(response).to have_http_status 302
+        expect(session[:user_id]).to eq(User.last.id)
+        expect(flash[:notice]).to eq("Signup successful!")
+        expect(response).to redirect_to users_path
+      end
+    end
+
+    context "when invalid params are passed" do
+      let :params do
+        {user: {username: "Tom", email: "t4tom@test.com"}}
+      end
+      it "responds with status code 200" do
+        post :create, params
+        expect(response).to have_http_status 200
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "when valid params are passed" do
+      before(:each) do
+        session[:user_id] = user.id
+        put :update, id: user.id, user: params
+        user.reload
+      end
+      let :params do
+        {email: "tomtest@tom.com"}
+      end
+      it "updates the specified user's attribute" do
+        expect(response).to have_http_status 302
+        expect(user.email).to eq params[:email]
+        expect(response).to redirect_to user_path(user.id)
+      end
+    end
+
+      context "when invalid params are passed" do
+        before(:each) do
+          session[:user_id] = nil
+          put :update, id: user.id, user: params
+          user.reload
+        end
+        let :params do
+          {email: "tomtest@tom.com"}
+        end
+        it "responds with a status 302" do
+        expect(response).to have_http_status 302
+        expect(user.bio).to_not eq params[:bio]
+        expect(response).to redirect_to new_session_path
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "logs out user, clears the session, and sets active to false" do
+      delete :destroy, {id: user.id}
+      expect(session[:user_id]).to eq nil
+      expect(assigns(:tasha)).to eq nil
+      expect(response).to redirect_to root_path
+    end
+  end
+
 end
