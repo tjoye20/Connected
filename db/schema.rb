291d62c891d64734063e5b6ee421148da3b962e4
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-06 14:19:12 -0500

    Update seed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903180119) do
+ActiveRecord::Schema.define(version: 20160906191543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160903180119) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image_url"
   end
 
 end
