ce552106b3af95e1040cce4ac426fbc02bdc244b
Jason Pelletier <jpelletier19@me.com>
2016-09-06 14:30:36 -0500

    add json rendering for user interest view

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -5,5 +5,10 @@ class InterestsController < ApplicationController
     render json: @interests
   end
 
+  def create
+    @interest = Interest.find_by(name: params["interest"]["name"])
+    @users = @interest.users
+    render json: @users
+  end
 
 end
