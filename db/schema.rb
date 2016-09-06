d126a22375a901d8792717630bf22b1b5dac4ac1
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-06 15:51:58 -0500

    Add user column to user model. Add migration. Update seed file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906204227) do
+ActiveRecord::Schema.define(version: 20160906204917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160906204227) do
     t.string   "city"
     t.string   "state"
     t.integer  "zipcode"
+    t.string   "bio"
   end
 
 end
