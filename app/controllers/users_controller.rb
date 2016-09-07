be6b39dec28760c0f2c7ee5105fca9bac4da6af3
Jason Pelletier <jpelletier19@me.com>
2016-09-06 20:11:33 -0500

    add route to search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,11 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def search
+    @users = User
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
