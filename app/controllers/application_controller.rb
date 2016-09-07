e924c5ddfd0bef866c0559c169d9183fbfdef412
Tolu Oyeniyi <tjoye20@gmail.com>
2016-09-07 16:13:12 -0500

    Update helpers in app controller and update sessions controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,62 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-
   # protect_from_forgery with: :null_sessions
 
-  def current_user
-    User.find_by(id: session[:user_id])
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
   end
 
-  def authenticate_user
-    if !current_user
-      redirect_to login_path, notice: "You must be signed in to do that!"
+  # Remembers a user in a persistent session.
+  # signed creates an encrypted cookie so that's why we use it.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
-  helper_method :current_user, :authenticate_user
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  helper_method :current_user, :log_in, :remember, :current_user?,
+  :current_user, :logged_in?, :forget, :log_out, :redirect_back_or
 end
