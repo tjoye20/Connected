ace1127890618c620a44d3227f55c78fea8b6d7e
Jessica Bohn <bohnjessica15@gmail.com>
2016-09-07 19:51:18 -0500

    Had to call sessions part of params
    For our login part of the sessions controller only the general
    params were being passed back to the login (create) action but
    the parameters were being created on the front-end inside of a
    sessions portion of the params, which is why the params[] calls
    were changed to params["session"][] to correctly obtain the
    necessary information.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params["session"][:email])
+    if user && user.authenticate(params["session"][:password])
       session[:user_id] = user.id
       redirect_to users_path, :notice => "Logged in!"
     else
